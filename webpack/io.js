92aca894888224fb72f622a97c6abd334dd6ff6a
yandongxu <nextocean@gmail.com>
2015-12-28 09:54:39 +0800

    fix watch mode output location

--- webpack/io.js
+++ webpack/io.js
@@ -15,7 +15,7 @@ module.exports = function(options) {
     const entry = Object.assign({ main: ['./src/scripts/main'] }, options.entry);
     const output = {
         publicPath: options.devServer ? '/' : 'assets/',
-        path: path.resolve(__dirname, (options.longTermCaching ? '../dist' : 'public'), 'assets'),
+        path: path.resolve(__dirname, (options.longTermCaching ? '../dist' : '../public'), 'assets'),
         // works: bundle all files
         // filename: options.longTermCaching ? 'scripts/bundle.[hash].js' : 'scripts/bundle.js',
         // chunks
@@ -29,46 +29,53 @@ module.exports = function(options) {
             test: /\.jsx?$/,
             loaders: ['babel'],
             include: path.resolve(__dirname, '../src')
-        }, {
+        },
+        {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap!postcss-loader', {
                 publicPath: '../'
             })
-        }, {
-            test: /\.(png|jpe?g|gif|svg)$/,
+        },
+        {
+            test: /\.(jpe?g|png|gif|svg)$/,
             loaders: [
-                'url-loader?limit=8192&name=images/[name].[ext]',
+                // TODO 某些情况下base64作为背景图片不能正常使用
+                // 'url-loader?limit=8192&name=images/[name].[ext]',
                 'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false',
                 'file-loader?name=images/[name].[ext]'
             ]
-        }, {
+        },
+        {
             test: /\.(woff|woff2)$/,
             loader: 'url-loader?limit=100000&name=fonts/[name].[ext]'
-        }, {
+        },
+        {
             test: /\.(ttf|eot)$/,
             loader: 'file-loader?name=fonts/[name].[ext]'
         }
     ];
-    const htmlWebpackConfig = {
-        devServer: options.devServer,
-        hash: false,
-        title: options.title || 'Next Starter Kit',
-        lang: options.lang || 'en',
-        inject: false,
-        template: './webpack/default.tpl.html'
-    };
     const plugins = [];
     const pages = [{ filename: 'index.html' }].concat(options.pages || []);
 
-    pages.forEach((page) => {
-        plugins.push(
-            new HtmlWebpackPlugin(Object.assign({}, htmlWebpackConfig, {
-                hash: false,
-                title: page.title || htmlWebpackConfig.title,
-                filename: (options.longTermCaching ? '../' : '') + page.filename
-            }))
-        );
-    });
+    if (options.watch) {
+        const htmlWebpackConfig = {
+            devServer: options.devServer,
+            hash: false,
+            title: options.title || 'Next Starter Kit',
+            lang: options.lang || 'en',
+            inject: false,
+            template: './webpack/default.tpl.html'
+        };
+        pages.forEach((page) => {
+            plugins.push(
+                new HtmlWebpackPlugin(Object.assign({}, htmlWebpackConfig, {
+                    hash: false,
+                    title: page.title || htmlWebpackConfig.title,
+                    filename: (options.longTermCaching ? '../' : '') + page.filename
+                }))
+            );
+        });
+    }
 
     if (options.minimiz) {
         plugins.push(
--- webpack/serve.js
+++ webpack/serve.js
@@ -2,6 +2,5 @@ module.exports = require('./io')({
     devtool: 'cheap-module-source-map',
     devServer: true,
     port: 9000,
-    debug: true,
-    watch: true
+    debug: true
 });
